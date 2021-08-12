a3067544421308e12c3eb2b71c8cd0bcd9e7ff96
Konstantinos Rakopoulos <k.rakopoulos@gmail.com>
2021-08-12 06:41:20 +0000

    Update first script

--- jenkins/systemGroovyScript.groovy
+++ jenkins/systemGroovyScript.groovy
@@ -1,1 +1,10 @@
-println(Jenkins.instance.pluginManager.plugins)
+import jenkins.model.*
+jenkins = Jenkins.instance
+
+def svr = jenkins.instance;
+
+def job = svr.getJob("TEST");
+
+def sched = job.scheduleBuild2(0);
+
+sched.get();
